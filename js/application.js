f020edb1a0e4f785cb5476ab416854efdda7e804
mager <andrew+github@mager.co>
2014-08-18 21:29:54 -0700

    Added Ember Data

--- js/application.js
+++ js/application.js
@@ -1,7 +1,22 @@
-window.Instaspot = Ember.Application.create({
+window.App = Ember.Application.create({
     LOG_TRANSITIONS: true,
     VERSION: '1.0.0',
     ready: function () {
-        console.log('App version: ' + Instaspot.VERSION + ' is ready.');
+        console.log('App version: ' + App.VERSION + ' is ready.');
     }
 });
+
+// Initializes Ember Data
+App.Store = DS.Store.extend({
+  adapter: DS.RESTAdapter
+})
+
+function grabImages(client_id) {  
+
+  var endpoint_popular = 'https://api.instagram.com/v1/media/popular?client_id=' + client_id
+
+}
+
+function onDataLoaded(instagram_data) {  
+
+}
--- js/router.js
+++ js/router.js
@@ -1,3 +1,7 @@
-Instaspot.Router.map(function() {
-  this.resource('instaspot', { path: '/' });
+// Create the global router to manage page state via URLs 
+App.Router.map(function() {
+  this.resource('index', { path: '/' });
 });
+
+// Create the default application route to set application-level state properties
+App.ApplicationRoute = Ember.Route.extend({});
