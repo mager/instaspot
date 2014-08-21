1ad115965d7a3536caa8c44589c1fc4321d5c0b9
mager <andrew+github@mager.co>
2014-08-21 14:29:54 -0700

    Get data from Instagram in the console

--- js/application.js
+++ js/application.js
@@ -1,5 +1,21 @@
 (function(){
+
+  var client_id = '451cf9fc4a0a44379d699e4ca48d58fb';
+
   var app = angular.module('instaspot', []);
+
+  app.factory("InstagramAPI", ['$http', function($http) {
+    return {
+      fetchPopular: function(callback){
+        var endpoint = "https://api.instagram.com/v1/media/popular?client_id=" + client_id + "&callback=JSON_CALLBACK";
+        $http.jsonp(endpoint).success(function(response){
+          callback(response.data);
+        });
+      }
+    }
+  }]);
+
+  // console.log(app);
 
   app.controller('SpotController', function(){
     this.products = spots;
@@ -23,5 +39,15 @@
    }
   ];
 
+  app.controller('DynamicImages', function($scope, InstagramAPI){
+    console.log('heyo');
+    $scope.data = {};
+
+    InstagramAPI.fetchPopular(function(data){
+
+      console.log(data);
+
+    });
+  });
 
 })();
