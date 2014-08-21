5a03c3bfed809e8d7b61d404f12d096991054785
mager <andrew+github@mager.co>
2014-08-21 12:51:30 -0700

    Fixed, plus introduction of CSS

--- js/application.js
+++ js/application.js
@@ -8,17 +8,17 @@
   var spots = [
    {
      id: 0,
-     image_href: 'http://photos-g.ak.instagram.com/hphotos-ak-xaf1/10616664_630976927000566_1003239505_n.jpg',
+     image_href: 'http://photos-g.ak.instagram.com/hphotos-ak-xaf1/10616664_630976927000566_1003239505_s.jpg',
      username: 'mager'
    },
    {
      id: 1,
-     image_href: 'http://photos-b.ak.instagram.com/hphotos-ak-xaf1/10593471_735752143130489_878680963_n.jpg',
+     image_href: 'http://photos-b.ak.instagram.com/hphotos-ak-xaf1/10593471_735752143130489_878680963_s.jpg',
      username: 'mager'
    },
    {
      id: 2,
-     image_href: 'http://photos-b.ak.instagram.com/hphotos-ak-xaf1/10593471_735752143130489_878680963_n.jpg',
+     image_href: 'http://scontent-a-pao.cdninstagram.com/hphotos-xaf1/t51.2885-15/10608039_579016542209237_1463884446_s.jpg',
      username: 'hilldogg6'
    }
   ];
