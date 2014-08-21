0422b99ada1acf9c58ae3342a772e9a529f6b4e7
mager <andrew+github@mager.co>
2014-08-21 12:42:55 -0700

    Multiple products

--- js/application.js
+++ js/application.js
@@ -1,12 +1,27 @@
 (function(){
   var app = angular.module('instaspot', []);
 
-  app.controller('ImageController', function(){
-    this.product = item;
+  app.controller('SpotController', function(){
+    this.products = spots;
   });
 
-  var item = {
-    image: 'http://photos-g.ak.instagram.com/hphotos-ak-xaf1/10616664_630976927000566_1003239505_s.jpg',
-    user: 'mager'
-  };
+  var spots = [
+   {
+     id: 0,
+     image_href: 'http://photos-g.ak.instagram.com/hphotos-ak-xaf1/10616664_630976927000566_1003239505_n.jpg',
+     username: 'mager'
+   },
+   {
+     id: 1,
+     image_href: 'http://photos-b.ak.instagram.com/hphotos-ak-xaf1/10593471_735752143130489_878680963_n.jpg',
+     username: 'mager'
+   },
+   {
+     id: 2,
+     image_href: 'http://photos-b.ak.instagram.com/hphotos-ak-xaf1/10593471_735752143130489_878680963_n.jpg',
+     username: 'hilldogg6'
+   }
+  ];
+
+
 })();
