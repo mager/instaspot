a48345a9ec6c106f13c61637e769b9d687745b11
mager <andrew+github@mager.co>
2014-08-20 23:39:37 -0700

    Import data correctly

--- js/application.js
+++ js/application.js
@@ -1,14 +1,12 @@
-var data = [
- {
-   image: 'http://photos-g.ak.instagram.com/hphotos-ak-xaf1/10616664_630976927000566_1003239505_n.jpg',
-   user: 'mager'
- },
- {
-   image: 'http://photos-b.ak.instagram.com/hphotos-ak-xaf1/10593471_735752143130489_878680963_n.jpg',
-   user: 'mager',
- },
- {
-   image: 'http://photos-b.ak.instagram.com/hphotos-ak-xaf1/10593471_735752143130489_878680963_n.jpg',
-   user: 'hilldogg6'
- }
-];
+(function(){
+  var app = angular.module('instaspot', []);
+
+  app.controller('ImageController', function(){
+    this.product = item;
+  });
+
+  var item = {
+    image: 'http://photos-g.ak.instagram.com/hphotos-ak-xaf1/10616664_630976927000566_1003239505_s.jpg',
+    user: 'mager'
+  };
+})();
